16060b536294442bfb4852d8b7278c54fb26c56d
rajkamalhm <rajkamalhm@gmail.com>
2020-02-13 12:17:17 +0530

    Create samplefile.java

---
+++ samplefile.java
@@ -0,0 +1,11 @@
+package com.curlpackage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class myclass {
+    public static void main(String[] args) {
+        SpringApplication.run(myclass.class, args);
+    }
+}
